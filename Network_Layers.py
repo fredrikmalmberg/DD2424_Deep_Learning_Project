83943a9e033020d8b803797a782201ebd9343c74
catweld <cfw56@cornell.edu>
2020-05-03 20:51:50 +0200

    Create Network_Layers.py

---
+++ Network_Layers.py
@@ -0,0 +1,27 @@
+import keras
+from keras import losses, models, layers, optimizers
+
+def buildLayers():
+    model = models.Sequential()
+    model.add(layers.InputLayer(input_shape=(256, 256, 3)))
+    model.add(layers.Conv2D(64, kernel_size = (3,3), activation='relu'))
+    model.add(layers.Conv2D(128, kernel_size = (3,3), activation='relu'))
+    model.add(layers.Conv2D(512, kernel_size = (3,3), activation='relu'))
+    model.add(layers.Conv2D(512, kernel_size = (3,3), activation='relu'))
+    model.add(layers.Conv2D(512, kernel_size = (3,3), activation='relu'))
+    model.add(layers.Conv2D(512, kernel_size = (3,3), activation='relu'))
+    model.add(layers.Conv2D(256, kernel_size = (3,3), activation='relu'))
+    model.add(layers.Flatten())
+    model.add(layers.Dense(1, activation='softmax'))
+    return model
+
+def computeLoss(y_true, y_pred):
+    loss = losses.categorical_crossentropy(y_true, y_pred, from_logits=False, label_smoothing=0)
+    return loss
+
+def trainNetwork(train_x, train_y, validation_x, validation_y, epochs_val, bsize):   
+    model = buildLayers()
+    SGD = optimizers.Adam(learning_rate=0.001)
+    model.compile(loss='sparse_categorical_crossentropy', optimizer=SGD, metrics=["accuracy"])
+    model.fit(train_x, train_y, epochs=epochs_val, batch_size=bsize, validation_data = (validation_x, validation_y))
+    return model
