7c1681eec6cd393ec2205b30491f8b499851b707
JohnMagnusson <johnvhmagnusson@gmail.com>
2020-05-06 12:23:28 +0200

    Finished the structure of the model, it is the same as in the paper.
    Next step is to add the probability distribution a,b so we can train the network. Now we have missmatch in dimension when training

--- Network_Layers.py
+++ Network_Layers.py
@@ -1,19 +1,70 @@
-import keras
-from keras import losses, models, layers, optimizers
+from keras import losses, models, layers, optimizers, regularizers
 
 
 def buildLayers(dimensions):
+    """
+    We use He initialization because it tends to give better results when using ReLu activation.
+    :param dimensions: Dimension of the input data
+    :return: A model
+    """
+    l2_reg = regularizers.l2()  # Todo find the value of the regularization in the paper, default here is 0.01
+
     model = models.Sequential()
     model.add(layers.InputLayer(input_shape=dimensions[1:]))
-    model.add(layers.Conv2D(64, kernel_size=(3, 3), activation='relu'))
-    model.add(layers.Conv2D(128, kernel_size=(3, 3), activation='relu'))
-    model.add(layers.Conv2D(512, kernel_size=(3, 3), activation='relu'))
-    model.add(layers.Conv2D(512, kernel_size=(3, 3), activation='relu'))
-    model.add(layers.Conv2D(512, kernel_size=(3, 3), activation='relu'))
-    model.add(layers.Conv2D(512, kernel_size=(3, 3), activation='relu'))
-    model.add(layers.Conv2D(256, kernel_size=(3, 3), activation='relu'))
-    model.add(layers.Flatten())
-    model.add(layers.Dense(1, activation='softmax'))
+    model.add(layers.Conv2D(64, (3, 3), activation='relu', kernel_initializer="he_normal", strides=(1, 1),
+                            padding='same', kernel_regularizer=l2_reg, name='conv1_1'))
+    model.add(layers.Conv2D(64, (3, 3), activation='relu', kernel_initializer="he_normal", strides=(2, 2),
+                            padding='same', kernel_regularizer=l2_reg, name='conv1_2'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.Conv2D(128, (3, 3), activation='relu', kernel_initializer="he_normal", strides=(1, 1),
+                            padding='same', kernel_regularizer=l2_reg, name='conv2_1'))
+    model.add(layers.Conv2D(128, (3, 3), activation='relu', kernel_initializer="he_normal", strides=(2, 2),
+                            padding='same', kernel_regularizer=l2_reg, name='conv2_2'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.Conv2D(256, (3, 3), activation='relu', kernel_initializer="he_normal", strides=(1, 1),
+                            padding='same', kernel_regularizer=l2_reg, name='conv3_1'))
+    model.add(layers.Conv2D(256, (3, 3), activation='relu', kernel_initializer="he_normal", strides=(1, 1),
+                            padding='same', kernel_regularizer=l2_reg, name='conv3_2'))
+    model.add(layers.Conv2D(256, (3, 3), activation='relu', kernel_initializer="he_normal", strides=(2, 2),
+                            padding='same', kernel_regularizer=l2_reg, name='conv3_3'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.Conv2D(512, (3, 3), activation='relu', kernel_initializer="he_normal", strides=(1, 1),
+                            padding='same', kernel_regularizer=l2_reg, name='conv4_1'))
+    model.add(layers.Conv2D(512, (3, 3), activation='relu', kernel_initializer="he_normal", strides=(1, 1),
+                            padding='same', kernel_regularizer=l2_reg, name='conv4_2'))
+    model.add(layers.Conv2D(512, (3, 3), activation='relu', kernel_initializer="he_normal", strides=(1, 1),
+                            padding='same', kernel_regularizer=l2_reg, name='conv4_3'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.Conv2D(512, (3, 3), activation='relu', kernel_initializer="he_normal", strides=(1, 1),
+                            padding='same', kernel_regularizer=l2_reg, name='conv5_1', dilation_rate=2))
+    model.add(layers.Conv2D(512, (3, 3), activation='relu', kernel_initializer="he_normal", strides=(1, 1),
+                            padding='same', kernel_regularizer=l2_reg, name='conv5_2', dilation_rate=2))
+    model.add(layers.Conv2D(512, (3, 3), activation='relu', kernel_initializer="he_normal", strides=(1, 1),
+                            padding='same', kernel_regularizer=l2_reg, name='conv5_3', dilation_rate=2))
+    model.add(layers.BatchNormalization())
+    model.add(layers.Conv2D(512, (3, 3), activation='relu', kernel_initializer="he_normal", strides=(1, 1),
+                            padding='same', kernel_regularizer=l2_reg, name='conv6_1', dilation_rate=2))
+    model.add(layers.Conv2D(512, (3, 3), activation='relu', kernel_initializer="he_normal", strides=(1, 1),
+                            padding='same', kernel_regularizer=l2_reg, name='conv6_2', dilation_rate=2))
+    model.add(layers.Conv2D(512, (3, 3), activation='relu', kernel_initializer="he_normal", strides=(1, 1),
+                            padding='same', kernel_regularizer=l2_reg, name='conv6_3', dilation_rate=2))
+    model.add(layers.BatchNormalization())
+    model.add(layers.Conv2D(256, (3, 3), activation='relu', kernel_initializer="he_normal", strides=(1, 1),
+                            padding='same', kernel_regularizer=l2_reg, name='conv7_1'))
+    model.add(layers.Conv2D(256, (3, 3), activation='relu', kernel_initializer="he_normal", strides=(1, 1),
+                            padding='same', kernel_regularizer=l2_reg, name='conv7_2'))
+    model.add(layers.Conv2D(256, (3, 3), activation='relu', kernel_initializer="he_normal", strides=(1, 1),
+                            padding='same', kernel_regularizer=l2_reg, name='conv7_3'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.UpSampling2D((2, 2)))    # We upsample one step before the layer, and then use a (1,1) stride
+    model.add(layers.Conv2D(128, (3, 3), activation='relu', kernel_initializer="he_normal", strides=(1, 1),
+                            padding='same', kernel_regularizer=l2_reg, name='conv8_1'))
+    model.add(layers.Conv2D(128, (3, 3), activation='relu', kernel_initializer="he_normal", strides=(1, 1),
+                            padding='same', kernel_regularizer=l2_reg, name='conv8_2'))
+    model.add(layers.Conv2D(128, (3, 3), activation='relu', kernel_initializer="he_normal", strides=(1, 1),
+                            padding='same', kernel_regularizer=l2_reg, name='conv8_3'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.Conv2D(2, (1, 1), activation='softmax', padding='same', name='pred'))
     return model
 
 
@@ -24,7 +75,9 @@ def computeLoss(y_true, y_pred):
 
 def trainNetwork(train_x, train_y, validation_x, validation_y, epochs_val, bsize):
     model = buildLayers(train_x.shape)
+    # Todo next step is to implement the probability distribution a,b to upsample the pictures so we can train with them
     SGD = optimizers.Adam(learning_rate=0.001)
-    model.compile(loss='categorical_crossentropy', optimizer=SGD, metrics=["accuracy"])
+    model.compile(loss='categorical_crossentropy', optimizer=SGD, metrics=[
+        "accuracy"])  # https://keras.io/losses/, check at the bottom at the page, "categorical_crossentropy is another term for multi-class log loss."
     model.fit(train_x, train_y, epochs=epochs_val, batch_size=bsize, validation_data=(validation_x, validation_y))
     return model
