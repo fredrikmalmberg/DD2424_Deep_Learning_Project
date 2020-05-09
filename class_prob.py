1244f49fd17cdcfaa151f2cf1518ae7e57194611
Lucas Gongora <lucas.q.gongora@gmail.com>
2020-05-09 17:55:59 +0200

    class probabilitiy

--- class_prob.py
+++ class_prob.py
@@ -3,7 +3,13 @@ import numpy as np
 
 
 # np.load()
-weights = np.random.normla(0, 1, ())
+predicted = np.random.normal(5, 0.1, (1, 64, 64, 313))
+t = 0.38
+
+num = np.exp(np.log(predicted)/t)
+denom = np.sum(num, axis=3)
+result = num/denom
+
 
 
 
